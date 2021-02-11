bde598cb83ede1776391a6394169f65ab22d7041
Sergey Yamshchikov <yamsergey@gmail.com>
2021-02-11 13:24:26 +0100

    Print out PAC results of proxy discovery

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -30,7 +30,7 @@ class _MyAppState extends State<MyApp> {
     String platformVersion;
     // Platform messages may fail, so we use a try/catch PlatformException.
     try {
-      platformVersion = (await PlatformProxy().getPlatformProxies(url: "https://google.com")).toString();
+      platformVersion = (await PlatformProxy().getPlatformProxies(url: "https://google.com")).getProxiesAsPac();
     } on PlatformException {
       platformVersion = 'Failed to get platform version.';
     }
